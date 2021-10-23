1a6718785bfc22e37078fb0c06ceba2039e83ee7
Sachin Sharma <sharma.sachin013@gmail.com>
2021-10-23 07:59:19 +0530

    Update AllRouters.js

--- src/Routers/AllRouters.js
+++ src/Routers/AllRouters.js
@@ -3,14 +3,11 @@ import { Route, Switch } from "react-router-dom";
 import Main from "./components/Main";
 const AllRouters = () => {
   return (
-    <Switch>
-      <Route exact path="/">
-        <CovidTracker />
-      </Route>
-      <Route exact path="/login">
-        <Login />
-      </Route>
-    </Switch>
+    return (
+    <div>
+      <Main />
+    </div>
+  );
   );
 };
 
